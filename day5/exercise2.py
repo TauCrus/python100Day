f40c8b0f417ea1b3d5591ede79ff2a62e81873b0
陈清泉 <taucrus@chenqingquandeMacBook-Pro.local>
2019-05-07 23:00:50 +0800

    update day5

--- day5/exercise2.py
+++ day5/exercise2.py
@@ -5,3 +5,22 @@
 恰好等于它本身
 '''
 
+import time
+import math
+
+# start = time.clock()
+start = time.perf_counter()
+
+for num in range(1, 10000):
+    sum = 0 
+    for factor in range(1, int(math.sqrt(num)) + 1):
+        if num % factor == 0:
+            sum += factor
+            if factor > 1 and num / factor != factor:
+                sum += num / factor   
+    if sum == num:
+        print(num)
+
+# end = time.clock()
+end = time.perf_counter()
+print('执行时间：', (end - start),'秒')
